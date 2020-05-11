a7766d5fce4efe28f5ff3cc6b6ff5167f46e8028
rlopezdev <rlopez@linagora.com>
2020-05-11 15:01:48 +0200

    edit docker entrypoint : rewrite of VueJS env files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -29,10 +29,10 @@ while [ "$1" != "" ]; do
             VUE_APP_NODERED_PASSWORD=$LINTO_STACK_BLS_PASSWORD" >.env.production
         else
             echo "VUE_APP_URL=
-            VUE_APP_TOCK_URL=https://$LINTO_STACK_DOMAIN/tock
+            VUE_APP_TOCK_URL=http://$LINTO_STACK_DOMAIN/tock
             VUE_APP_TOCK_USER=$LINTO_STACK_TOCK_USER
             VUE_APP_TOCK_PASSWORD=$LINTO_STACK_TOCK_PASSWORD
-            VUE_APP_NODERED=https://$LINTO_STACK_DOMAIN/redui
+            VUE_APP_NODERED=http://$LINTO_STACK_DOMAIN/redui
             VUE_APP_NODERED_USER=$LINTO_STACK_BLS_USER
             VUE_APP_NODERED_PASSWORD=$LINTO_STACK_BLS_PASSWORD" >.env.production
         fi
@@ -53,10 +53,10 @@ while [ "$1" != "" ]; do
             VUE_APP_NODERED_PASSWORD=$LINTO_STACK_BLS_PASSWORD" >.env.development
         else
             echo "VUE_APP_URL=
-            VUE_APP_TOCK_URL=https://$LINTO_STACK_DOMAIN/tock
+            VUE_APP_TOCK_URL=http://$LINTO_STACK_DOMAIN/tock
             VUE_APP_TOCK_USER=$LINTO_STACK_TOCK_USER
             VUE_APP_TOCK_PASSWORD=$LINTO_STACK_TOCK_PASSWORD
-            VUE_APP_NODERED=https://$LINTO_STACK_DOMAIN/redui
+            VUE_APP_NODERED=http://$LINTO_STACK_DOMAIN/redui
             VUE_APP_NODERED_USER=$LINTO_STACK_BLS_USER
             VUE_APP_NODERED_PASSWORD=$LINTO_STACK_BLS_PASSWORD" >.env.development
         fi
